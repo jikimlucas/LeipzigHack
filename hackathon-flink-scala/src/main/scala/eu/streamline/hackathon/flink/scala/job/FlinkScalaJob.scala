65650c5bea75a8e25d50a1454e42071656b58e2e
jikimlucas <jikimlucas@me.com>
2018-06-30 22:34:58 +0200

    common pom & aggregate update

--- hackathon-flink-scala/src/main/scala/eu/streamline/hackathon/flink/scala/job/FlinkScalaJob.scala
+++ hackathon-flink-scala/src/main/scala/eu/streamline/hackathon/flink/scala/job/FlinkScalaJob.scala
@@ -4,11 +4,9 @@ import java.util.Date
 
 import eu.streamline.hackathon.common.data.GDELTEvent
 import eu.streamline.hackathon.flink.operations.GDELTInputFormat
-import org.apache.flink.api.common.functions.FoldFunction
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.core.fs.Path
 import org.apache.flink.streaming.api.TimeCharacteristic
-import org.apache.flink.streaming.api.functions.sink.SocketClientSink
 import org.apache.flink.streaming.api.functions.timestamps.BoundedOutOfOrdernessTimestampExtractor
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.scala.function.WindowFunction
@@ -17,6 +15,9 @@ import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.streaming.api.windowing.windows.TimeWindow
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema
 import org.apache.flink.util.Collector
+import org.apache.flink.api.common.functions._
+import org.apache.flink.api.common.state.{AggregatingStateDescriptor, FoldingStateDescriptor, ListStateDescriptor, ReducingStateDescriptor}
+
 
 
 object FlinkScalaJob {
@@ -25,7 +26,7 @@ object FlinkScalaJob {
 
     val parameters = ParameterTool.fromArgs(args)
     val pathToGDELT = "/Users/jlucas/Documents/leipzig/180-days.csv"
-    val country = parameters.get("country", "USA")
+   // val country = parameters.get("country", "USA")
 
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
@@ -48,15 +49,11 @@ object FlinkScalaJob {
     }).keyBy((event: GDELTEvent) => {
       event.actor1Code_countryCode
     }).window(TumblingEventTimeWindows.of(Time.days(1)))
-
-    .fold(
+    .aggregate(new SumAggregate(), new SumWindowFunction())
+/*    .fold(
         0.0,
-       // "",
-      //Tuple2(0.0, 0),
         new FoldFunction[GDELTEvent, Double] {
           override def fold(accumulator: Double, value: GDELTEvent) = {
-       // override def fold(accumulator: Tuple2, value: GDELTEvent) = {
-           // accumulator + value.avgTone
             accumulator
           }
         },
@@ -74,23 +71,48 @@ object FlinkScalaJob {
             out.collect(output)
           }
         }
-    )
-    .writeToSocket("localhost",5555, new SimpleStringSchema())
-    //  writeToSocket("localhost", 9000, new SimpleStringSchema )
+    )*/
 
-/**
-   val consumer_stream = env.socketTextStream("localhost", 5555, '\n')
+   .writeToSocket("localhost",5555, new SimpleStringSchema())
 
-    env.socketTextStream("localhost", 5555)
-      .map(filtered_source => { println(filtered_source); filtered_source })
-      .addSink(new SocketClientSink[String]("localhost", 5555, new SimpleStringSchema))
-**/
-    //filtered_source.print();
+  //  filtered_source.print()
 
-    //
 
     env.execute("Flink Scala GDELT Analyzer")
 
   }
 
+  class SumAggregate extends AggregateFunction[GDELTEvent, (Double, Integer), (Double, Integer)] {
+
+    override def createAccumulator(): (Double, Integer) = (0.0, 0)
+
+    override def merge(a: (Double, Integer), b: (Double, Integer)): (Double, Integer) = (a._1+b._1, a._2 + b._2)
+
+    override def getResult(accumulator: (Double, Integer)): (Double, Integer) = accumulator
+
+    override def add(value: GDELTEvent, accumulator: ( Double, Integer))  = (
+      accumulator._1 + value.avgTone, accumulator._2 +1)
+  }
+
+  class SumWindowFunction
+    extends WindowFunction[(Double, Integer), (String), String, TimeWindow] {
+
+    override def apply(
+                        key: String,
+                        window: TimeWindow,
+                        input: Iterable[(Double, Integer)],
+                       // out: Collector[(String, Double, Integer, String)]): Unit = {
+                        out: Collector[(String)]): Unit = {
+      val format = new java.text.SimpleDateFormat("yyyy-MM-dd")
+      val formated_date = format.format(new Date(window.getStart))
+
+      input.foreach(e => {
+        val output = key + "|" + e._1 + "|" + e._2 + "|" +  formated_date + "\n"
+        out.collect(
+          (output)
+        )
+      }
+      )
+    }
+  }
 }
